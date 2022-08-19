e6f720ec355948590477cbacbefbead83cbb5ccb
KegFmn <48885939+KegFmn@users.noreply.github.com>
2022-08-19 12:51:36 +0800

    Update MonitorTask.java
    优化数据库定时写入评论数

--- src/main/java/com/likc/task/MonitorTask.java
+++ src/main/java/com/likc/task/MonitorTask.java
@@ -1,6 +1,7 @@
 package com.likc.task;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.likc.entity.Monitor;
 import com.likc.service.MonitorService;
 import com.likc.util.RedisUtils;
@@ -8,6 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 /**
  * @author likc
@@ -22,14 +26,29 @@ public class MonitorTask {
 
     @Autowired
     private MonitorService monitorService;
+    
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Scheduled(cron = "0 0 0/1 * * ? ")
     public void monitorTask() {
         log.info("定时任务写入数据库开始");
         Monitor monitor = new Monitor();
+        
+        RestTemplate restTemplate = new RestTemplate();
+        StringBuilder builder = new StringBuilder("https://api.github.com");
+        builder.append("/repos/KegFmn/vueblog-vuetify/issues/2");
+        Map<String, Object> map = null;
+        try {
+            String object = restTemplate.getForObject(builder.toString(), String.class);
+            map = objectMapper.readValue(object, Map.class);
+        } catch (Exception e) {
+            log.error("同步github评论失败，异常={}", e.getMessage());
+        }
+        int blessTotal = map.get("comments") == null ? 0 : (Integer) map.get("comments");
 
         monitor.setVisitTotal(Long.valueOf(redisUtils.get("visitTotal").toString()));
-        monitor.setBlessTotal(Long.valueOf(redisUtils.get("blessTotal").toString()));
+        monitor.setBlessTotal(blessTotal == 0 ? Long.valueOf(redisUtils.get("blessTotal").toString()) : (long)blessTotal);
         monitor.setBlogTotal(Long.valueOf(redisUtils.get("blogTotal").toString()));
 
         QueryWrapper<Monitor> wrapper = new QueryWrapper<>();
